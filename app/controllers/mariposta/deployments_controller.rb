be0e5629448ebea9af3bdbb999c4589be00ff5a6
Jared White <jared@jaredwhite.com>
2018-03-29 16:22:30 -0700

    action, not filter =)

--- app/controllers/mariposta/deployments_controller.rb
+++ app/controllers/mariposta/deployments_controller.rb
@@ -1,6 +1,6 @@
 class Mariposta::DeploymentsController < ApplicationController
   skip_before_action :authenticate_user!, raise: false
-  skip_before_filter :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
 
   def new
     render json: {status: 'Use POST instead, please!'}
--- lib/mariposta-core/version.rb
+++ lib/mariposta-core/version.rb
@@ -1,3 +1,3 @@
 module MaripostaCore
-  VERSION = '4.1.2'
+  VERSION = '4.1.3'
 end
